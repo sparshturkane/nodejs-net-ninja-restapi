8ce2bc6c656a81fc4b1c1a84c99394bdcc9d2942
sparshturkane <rajaturkane99@gmail.com>
2017-11-27 14:22:28 +0530

    requesting external apis

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ var routes = require('./routes/api');
 var app = express();
 
 // connect to mongodb
-mongoose.connect('mongodb://localhost/ninjago', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/ninjago', {useMongoClient: true});
 mongoose.Promise = global.Promise; // using global promise because mongoose promise is depricated
 
 // body parser
 app.use(bodyParser.json());
 
 // initialize routes
-app.use('/api',routes);
+app.use('/api', routes);
 
 // error handling middleware
 app.use(function (err, req, res, next) {
-  res.status(422).send({error: err.message});
+    res.status(422).send({error: err.message});
 });
 
 // listen for requests
 app.listen(process.env.port || 3000, function () {
-  console.log('listening on 3000')
+    console.log('listening on 3000')
 });
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var Ninja = require('../models/ninja');
+var request = require('request');
 
 /**
  * initilizing core modules
@@ -12,37 +13,46 @@ var router = express.Router();
 
 // getting list of all ninjas from db
 router.get('/ninjas', function (req, res, next) {
-  Ninja.find().then(function (ninja) {
-    res.send(ninja);
-  });
+    Ninja.find().then(function (ninja) {
+        res.send(ninja);
+    });
 });
 
 // inserting ninja in db
 router.post('/ninjas', function (req, res, next) {
-  Ninja.create(req.body).then(function (ninja) {
-    res.send(ninja);
-  }).catch(next);
+    Ninja.create(req.body).then(function (ninja) {
+        res.send(ninja);
+    }).catch(next);
 });
 
 // updating ninja in db
 router.put('/ninjas/:id', function (req, res, next) {
-  Ninja.findByIdAndUpdate({
-    _id: req.params.id
-  }, req.body).then(function () {
-    Ninja.findOne({
-      _id: req.params.id
-    }).then(function (ninja) {
-      res.send(ninja);
+    Ninja.findByIdAndUpdate({
+        _id: req.params.id
+    }, req.body).then(function () {
+        Ninja.findOne({
+            _id: req.params.id
+        }).then(function (ninja) {
+            res.send(ninja);
+        });
     });
-  });
 });
 
 // deleting ninja from db
 router.delete('/ninjas/:id', function (req, res, next) {
-  Ninja.findByIdAndRemove({
-    _id: req.params.id
-  }).then(function (ninja) {
-    res.send(ninja);
+    Ninja.findByIdAndRemove({
+        _id: req.params.id
+    }).then(function (ninja) {
+        res.send(ninja);
+    });
+});
+
+// get/
+router.get('/', function (req, res, next) {
+  request('http://www.google.com', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred and handle it
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    res.send(body)
   });
 });
 
